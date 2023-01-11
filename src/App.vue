5a1ffe9fd5850944232012e5b194460095266415
letovaatiga <letovaatiga@gmail.com>
2023-01-11 15:32:40 +0400

    Added pages header

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,33 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <div class="app-container" :class="authStore.user">
+  <div class="app-container" :class="authStore.user, checkPath('/home') ? '' : 'pg-header'">
     <Alert />
     <RouterView />
   </div>  
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+          currentRoute: ''
+      };
+    },
+    watch: {
+      $route (to, from) {
+        this.currentRoute = to.path;
+      }
+    },
+    mounted() {
+      this.currentRoute = this.$route.path;
+    },
+    methods: {
+      checkPath(path) {
+        if(this.currentRoute === path) {
+            return true;
+        }
+      }
+    }
+  }
+</script>
--- src/components/Dashboard/Layout/Header.vue
+++ src/components/Dashboard/Layout/Header.vue
@@ -2,16 +2,28 @@
 import { useAuthStore } from '@/stores';
 
 const authStore = useAuthStore();
-
 </script>
 
 <template>
-    <header class="navbar ms-auto fixed-top  flex-md-nowrap">
+    <header class="navbar ms-auto fixed-top flex-md-nowrap">
         <div class="container-fluid">
             <img class="ms-3 my-0" height="48" src="@/assets/naviwell-logo.png" data-bs-toggle="collapse"
                 data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
                 aria-label="Toggle navigation" />
-            <div class="d-flex flex-row ms-auto gap-1 mx-2 user-block">
+                <div class="header-datetime">
+                    <span>{{currentTime()}}</span>
+                    <span>{{currentDate()}}</span>
+                </div>
+                <div class="header-logo">
+                    <div class="top-block-logo">
+                        <img src="@/assets/img/variohealth-logo.png" alt="VarioHealth Logo" />
+                        <div class="top-block-info_text">
+                            <span>VarioHealth</span>
+                            <span>Advanced Integrative Medicine</span>
+                        </div>
+                    </div>
+                </div>
+            <div class="d-flex flex-row gap-1 user-block">
                 <img src="https://github.com/mdo.png" alt="mdo" width="48" height="48" class="rounded-circle">
                 <div class="text-center">
                     <!-- <a href="#" class="nav-link link-dark px-2 active" aria-current="page">{{authStore.user?.name}}</a> -->
@@ -22,3 +34,29 @@ const authStore = useAuthStore();
         </div>
     </header>
 </template>
+
+<script>
+    export default {
+    methods: {
+        currentDate() {
+        let date = new Date();
+        let currentWeekday = date.toLocaleString('en-us', {weekday: 'short'});
+        let currentMonth = date.toLocaleString('en-us', {month: 'short'});
+        let currentDay = date.toLocaleString('en-us', {day: 'numeric'});
+        let currentYear = date.toLocaleString('en-us', {year: 'numeric'});
+        let currentDate = currentWeekday + ' ' + currentDay + ' ' + currentMonth + ' ' + currentYear;
+        return currentDate;
+        },
+
+        currentTime() {
+        let date = new Date();
+        let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+        let am_pm = date.getHours() >= 12 ? "pm" : "am";
+        hours = hours < 10 ? "0" + hours : hours;
+        let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+        let currentTime = hours + ":" + minutes + " " + am_pm;
+        return currentTime;
+        }
+    }
+    };
+</script>
