879b979d4a66990038c0be6ce4ff84280a06f893
samadovdenis@gmail.com <samadovdenis@gmail.com>
2023-01-30 12:34:48 +0400

    Messages logic wip

---
+++ src/helpers/fileApi.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useAuthStore } from '@/stores';
+import {generateUUID, getFileExt} from './file.js'
+
+export const axiosFileInstance = axios.create({
+    baseURL: `${import.meta.env.VITE_FILEAPI_URL}`
+});
+
+axiosFileInstance.interceptors.request.use((request) => {
+    const { claim } = useAuthStore();
+    const isLoggedIn = !!claim?.AccessToken;
+    const isFileUrl = request.baseURL.startsWith(import.meta.env.VITE_FILEAPI_URL);
+    if (isLoggedIn && isFileUrl) {
+        request.headers.Authorization = `Bearer ${claim.AccessToken}`
+    }
+    return request
+})
+
+async function getFileUrlFromRef(type, ref) {
+    const result = await axiosFileInstance.get(`/${type}/${ref}`)
+    return result.data.body
+}
+
+export async function downloadFile(file, ref, type) {
+    const a = document.createElement("a");
+    a.download = file.name;
+    const tempUrl = await getFileUrlFromRef(type, ref);
+    a.href = tempUrl;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+export function uploadFile(file, type, path) {
+    const originalname = file.name;
+    const filename = generateUUID() + '.' + getFileExt(originalname);
+    const uploader = axiosFileInstance.put(`/${type}/${path}/${filename}`, file, {
+        headers: {
+            'Content-Type': file.type ? file.type : 'application/octet-stream'
+        }
+    })
+
+    return {
+        name: originalname,
+        ref: filename,
+        axios: uploader
+    }
+}
--- src/views/MessagesView.vue
+++ src/views/MessagesView.vue
@@ -5,6 +5,14 @@ import UserIcon from '@/components/icons/IconUser.vue'
 import FileIcon from '@/components/icons/IconFile.vue'
 import DownloadIcon from '@/components/icons/IconDownload.vue'
 import PatientDetails from '@/components/Patient/PatientDetails.vue'
+import { Form, Field } from 'vee-validate';
+
+import { axiosInstance, downloadFile, uploadFile } from '@/helpers';
+import { useAlertStore } from '@/stores';
+import userMixin from '@/mixins/user.js'
+
+import _forEach from 'lodash/forEach';
+import _findIndex from 'lodash/findIndex'
 
 export default {
     components: {
@@ -13,14 +21,73 @@ export default {
         UserIcon,
         FileIcon,
         DownloadIcon,
-        PatientDetails
+        PatientDetails,
+        Form,
+        Field
     },
+    mixins: [
+        userMixin,
+    ],
     data() {
+        const alertStore = useAlertStore()
         return {
+            alertStore,
             isModalVisible: false,
+            list: [],
+            selectedPatient: null,
+            messages: [],
         }
     },
+    computed: {
+        localDate() {
+            return (time) => new Date(time).format('DD.MM')
+        },
+        time() {
+            return (time) => new Date(time).format('DD.MM.YYYY hh:mm')
+        },
+        getMessageType() {
+            return (msgObject) => {
+                if (msgObject.attachments.length > 0) {
+                    return 'attachment'
+                }
+                return 'message'
+            }
+        },
+    },
+    watch: {
+        selectedPatient: {
+            handler(value) {
+                if (value != null) {
+                    this.getMessagesForPatient(value.id)
+                }
+            }
+        },
+        messages: {
+            handler() {
+                const container = this.$refs.chat;
+                this.$nextTick(() => {
+                    container.scrollTop = container.scrollHeight;
+                });
+            },
+            deep: true,
+        },
+    },
     methods: {
+        timeAgo(value) {
+            const seconds = Math.floor((new Date().getTime() - new Date(value).getTime()) / 1000)
+            let interval = seconds / 31536000
+            const rtf = new Intl.RelativeTimeFormat("en", { numeric: 'auto', style: 'short' })
+            if (interval > 1) { return rtf.format(-Math.floor(interval), 'year') }
+            interval = seconds / 2592000
+            if (interval > 1) { return rtf.format(-Math.floor(interval), 'month') }
+            interval = seconds / 86400
+            if (interval > 1) { return rtf.format(-Math.floor(interval), 'day') }
+            interval = seconds / 3600
+            if (interval > 1) { return rtf.format(-Math.floor(interval), 'hour') }
+            interval = seconds / 60
+            if (interval > 1) { return rtf.format(-Math.floor(interval), 'minute') }
+            return rtf.format(-Math.floor(interval), 'second')
+        },
         showModal(event, e) {
             this.selectedEvent = event
             this.isModalVisible = !this.isModalVisible;
@@ -28,10 +95,102 @@ export default {
                 e.stopPropagation()
             }
         },
+        populateMessages(responseMessage) {
+            this.messages.push({
+                id: responseMessage.id,
+                from: responseMessage.from,
+                body: responseMessage.body,
+                time: responseMessage.created_at
+            })
+        },
+        getMessagesList() {
+            axiosInstance.get('/messages')
+                .then(response => {
+                    console.log(response.data)
+                    response.data.messages.forEach(element => {
+                        this.list.push({
+                            patient: element.patient,
+                            patient_id: element.patient_id,
+                            name: this.userName(element.patient.user),
+                            message: element.body,
+                            date: element.created_at
+                        })
+                    });
+
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.alertStore.error(error.response.data.message)
+                });
+        },
+        getMessagesForPatient(id) {
+            axiosInstance.get(`/messages/${id}`)
+                .then(response => {
+                    this.messages = []
+                    response.data.messages.forEach(async element => {
+                        this.populateMessages(element)
+                    });
+
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.alertStore.error(error.response.data.message)
+                });
+        },
+        selectChat(patient) {
+            this.selectedPatient = patient
+        },
+        selectFile() {
+            const r = this.$refs.fileUpload;
+            r.click();
+        },
+        addFile(event) {
+            const files = event.target.files
+            this.$refs.messageForm.setFieldValue('files', files)
+        },
+        async clickedDownload(file) {
+            this.alertStore.success(`Downloading ${file.name}`)
+            downloadFile(file, `${this.selectedPatient.id}/${file.ref}`, 'messages')
+        },
+        submitMessage(values) {
+
+            // No values specified
+            if (!values.message && !values.files) {
+                return
+            }
+
+            const formBody = {
+                attachments: [],
+                message: values.message
+            }
+
+            _forEach(values.files, (element => {
+                const fileData = uploadFile(element, 'messages', this.selectedPatient.id)
+                formBody.attachments.push({
+                    name: fileData.name,
+                    ref: fileData.ref
+                })
+            }));
+
+            axiosInstance.post("/messages", { body: formBody, patient_id: this.selectedPatient.id })
+                .then(response => {
+                    const element = response.data.message;
+                    this.populateMessages(element)
+
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.alertStore.error(error.response.data.message)
+                });
+
+            this.$refs.messageForm.setValues({})
+
+
+        },
+
     },
     mounted() {
-        const container = this.$refs.chat;
-        container.scrollTop = container.scrollHeight;
+        this.getMessagesList()
     }
 }
 </script>
@@ -62,88 +221,36 @@ export default {
                 <div class="chat-content-wrapper">
                     <div class="chat-content-inner">
                         <ul class="chat-list">
-                            <li class="chat-list-item active-chat">
+                            <li :class="msg.patient_id == selectedPatient?.id ? 'active-chat' : ''"
+                                class="chat-list-item" v-for="msg in list" @click="selectChat(msg.patient)">
                                 <div class="chat-list-item-img">
                                     <img src="@/assets/img/image.png" alt="Patient photo" />
                                 </div>
                                 <div class="chat-list-item-content">
-                                    <div class="chat-list-item-content-name">Howard Aarons</div>
-                                    <div class="chat-list-item-content-message">howard-diet.pdf</div>
+                                    <div class="chat-list-item-content-name">{{ msg.name }}</div>
+                                    <div class="chat-list-item-content-message">{{ getMessageType(msg.message) ==
+                                    'attachment' ? msg.message.attachments[0].name : msg.message.message}}
+                                    </div>
                                 </div>
                                 <div class="chat-list-item-info">
-                                    <div class="chat-list-item-info-date">fri</div>
-                                    <div class="chat-list-item-info-icon">
+                                    <div class="chat-list-item-info-date">{{ localDate(msg.date) }}</div>
+                                    <div v-if="getMessageType(msg.message) == 'attachment'"
+                                        class="chat-list-item-info-icon">
                                         <AttachIcon />
                                     </div>
                                 </div>
                             </li>
-                            <li class="chat-list-item">
-                                <div class="chat-list-item-img">
-                                    <img src="@/assets/img/image.png" alt="" />
-                                </div>
-                                <div class="chat-list-item-content">
-                                    <div class="chat-list-item-content-name">Karen Bartley</div>
-                                    <div class="chat-list-item-content-message">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, atque!</div>
-                                </div>
-                                <div class="chat-list-item-info">
-                                    <div class="chat-list-item-info-date">12.01</div>
-                                    <div class="chat-list-item-info-icon"></div>
-                                </div>
-                            </li>
-                            <li class="chat-list-item">
-                                <div class="chat-list-item-img">
-                                    <img src="@/assets/img/image.png" alt="" />
-                                </div>
-                                <div class="chat-list-item-content">
-                                    <div class="chat-list-item-content-name">Edward Alvarez</div>
-                                    <div class="chat-list-item-content-message">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, atque!</div>
-                                </div>
-                                <div class="chat-list-item-info">
-                                    <div class="chat-list-item-info-date">10.01</div>
-                                    <div class="chat-list-item-info-icon">
-                                        <span>2</span>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="chat-list-item">
-                                <div class="chat-list-item-img">
-                                    <img src="@/assets/img/image.png" alt="" />
-                                </div>
-                                <div class="chat-list-item-content">
-                                    <div class="chat-list-item-content-name">Emily Atilla</div>
-                                    <div class="chat-list-item-content-message">howard-diet.pdf</div>
-                                </div>
-                                <div class="chat-list-item-info">
-                                    <div class="chat-list-item-info-date">09.01</div>
-                                    <div class="chat-list-item-info-icon">
-                                        <AttachIcon />
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="chat-list-item">
-                                <div class="chat-list-item-img">
-                                    <img src="@/assets/img/image.png" alt="" />
-                                </div>
-                                <div class="chat-list-item-content">
-                                    <div class="chat-list-item-content-name">Courtney Bailey</div>
-                                    <div class="chat-list-item-content-message">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, atque!</div>
-                                </div>
-                                <div class="chat-list-item-info">
-                                    <div class="chat-list-item-info-date">02.01</div>
-                                    <div class="chat-list-item-info-icon"><span>1</span></div>
-                                </div>
-                            </li>
-                        </ul> 
+                        </ul>
                     </div>
-                    
+
                 </div>
             </div>
 
-            <div class="chat-right">
+            <div v-show="selectedPatient" class="chat-right">
                 <div class="chat-right-head">
                     <div class="chat-right-head-info">
                         <img src="@/assets/img/image.png" alt="Patient photo" />
-                        <div class="chat-right-head-info-name">Howard Aarons</div>
+                        <div class="chat-right-head-info-name">{{ userName(selectedPatient?.user) }}</div>
                     </div>
 
                     <div class="chat-right-head-details" @click="showModal()">
@@ -160,52 +267,44 @@ export default {
                         <div class="chat-main">
                             <div class="messages-inner">
                                 <ul class="messages-list" ref="chat">
-                                    <li class="physician-message">
+                                    <li :class="message.from == selectedPatient.id ? '' : 'physician-message'"
+                                        v-for="message in messages">
                                         <UserIcon />
                                         <div class="message-list-text">
-                                            <div class="message-list-bubble">Good Morning, mr. Aarons! How are you feeling today?</div>
-                                            <div class="message-list-time">27.01.2023 15:00</div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <img src="@/assets/img/image.png" alt="Patient photo" />
-                                        <div class="message-list-text">
-                                            <div class="message-list-bubble">Good afternoon! I'm fine.</div>
-                                            <div class="message-list-time">27.01.2023 15:01</div>
-                                        </div>
-                                    </li>
-                                    <li class="physician-message">
-                                        <UserIcon />
-                                        <div class="message-list-text">
-                                            <div class="message-list-bubble">In the attached file you can find information about your prescribed diet. Have a nice day!</div>
-                                            <div class="message-list-bubble attach">
+                                            <div v-if="message.body.message != null" class="message-list-bubble">{{
+                                                message.body.message
+                                            }}</div>
+                                            <div v-for="att in message.body.attachments"
+                                                class="message-list-bubble attach" @click="clickedDownload(att)">
                                                 <div class="download-icon">
                                                     <FileIcon />
                                                 </div>
-                                                <a href="#">howard-diet.pdf</a>
-
-                                                <a href="#" class="attach-icon">
-                                                   <DownloadIcon /> 
-                                                </a>
+                                                {{ att.name }}
+                                                <DownloadIcon />
                                             </div>
-                                            <div class="message-list-time">27.01.2023 15:03</div>
+                                            <div class="message-list-time">{{ time(message.time) }}</div>
                                         </div>
                                     </li>
+
                                 </ul>
                             </div>
 
                             <div class="send-inner">
-                                <div class="send-inner-text">
-                                    <textarea placeholder="Write a message..."></textarea>
-                                </div>
+                                <Form @submit="submitMessage" ref="messageForm">
+                                    <div class="send-inner-text">
+                                        <Field as="textarea" name="message" placeholder="Write a message..." />
+                                    </div>
 
-                                <div class="send-inner-btns">
-                                    <button class="attach-btn">
-                                        <AttachIcon />
-                                    </button>
+                                    <div class="send-inner-btns">
+                                        <input hidden type="file" name="attachment" @change="addFile"
+                                            ref="fileUpload" />
+                                        <button type="button" @click="selectFile()" class="attach-btn">
+                                            <AttachIcon />
+                                        </button>
 
-                                    <button class="w-btn send-btn">Send</button>
-                                </div>
+                                        <button type="submit" class="w-btn send-btn">Send</button>
+                                    </div>
+                                </Form>
                             </div>
                         </div>
                     </div>
