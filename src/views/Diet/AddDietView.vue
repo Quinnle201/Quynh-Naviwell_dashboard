414e9dc4c52ce493af69c5fc6084830fcb00edc4
samadovdenis@gmail.com <samadovdenis@gmail.com>
2023-02-01 17:09:40 +0400

    Fix root element on diets

--- src/views/Diet/AddDietView.vue
+++ src/views/Diet/AddDietView.vue
@@ -4,7 +4,7 @@ import CameraIcon from '@/components/icons/IconCamera.vue'
 
 export default {
     components: {
-        AddIcon, 
+        AddIcon,
         CameraIcon
     },
     data() {
@@ -12,7 +12,7 @@ export default {
             days: 5,
         }
     },
-    methods: { 
+    methods: {
         addDay: function () {
             if (this.days < 10) this.days++;
         }
@@ -25,68 +25,71 @@ export default {
         <div class="layout-wrapper">
             <h3>Add New Diet</h3>
         </div>
-    </div>
 
-    <div class="add-diet-wrapper page-bg">
-        <div class="add-diet-head">
-            <div class="add-diet upload-photo">
-                <label>Upload Photo</label>
-                <div>
-                    <CameraIcon />
+
+        <div class="add-diet-wrapper page-bg">
+            <div class="add-diet-head">
+                <div class="add-diet upload-photo">
+                    <label>Upload Photo</label>
+                    <div>
+                        <CameraIcon />
+                    </div>
+                </div>
+
+                <div class="add-diet-inner">
+                    <div class="add-diet">
+                        <label for="diet">Diet Title</label>
+                        <input name="diet" type="text" placeholder="e.g. Mediterranean Diet" />
+                    </div>
+
+                    <div class="add-diet">
+                        <label for="duration">Diet Duration</label>
+                        <input name="duration" type="text" placeholder="e.g. 5 days" />
+                    </div>
                 </div>
             </div>
-            
-            <div class="add-diet-inner">
-                <div class="add-diet">
-                    <label for="diet">Diet Title</label>
-                    <input name="diet" type="text" placeholder="e.g. Mediterranean Diet" />
-                </div>
-                
-                <div class="add-diet">
-                    <label for="duration">Diet Duration</label>
-                    <input name="duration" type="text" placeholder="e.g. 5 days" />
-                </div>
-            </div>
-        </div>
 
-        <div class="add-diet-day-grid">
-            <div class="add-diet-day-grid-item" v-for="(day, index) in days" :key="day" :id="day">
-                <div class="add-diet-day-grid-item title">Day {{ index + 1 }}</div>
+            <div class="add-diet-day-grid">
+                <div class="add-diet-day-grid-item" v-for="(day, index) in days" :key="day" :id="day">
+                    <div class="add-diet-day-grid-item title">Day {{ index + 1 }}</div>
 
-                <div class="add-diet">
-                    <label>Breakfast</label>
-                    <textarea placeholder="e.g. Greek yogurt with strawberries and chia seeds"></textarea>
-                </div>
+                    <div class="add-diet">
+                        <label>Breakfast</label>
+                        <textarea placeholder="e.g. Greek yogurt with strawberries and chia seeds"></textarea>
+                    </div>
 
-                <div class="add-diet">
-                    <label>Lunch</label>
-                    <textarea placeholder="e.g. A whole grain sandwich with hummus and vegetables"></textarea>
-                </div>
+                    <div class="add-diet">
+                        <label>Lunch</label>
+                        <textarea placeholder="e.g. A whole grain sandwich with hummus and vegetables"></textarea>
+                    </div>
 
-                <div class="add-diet">
-                    <label>Dinner</label>
-                    <textarea placeholder="e.g. A tuna salad with greens and olive oil, as well as a fruit salad"></textarea>
+                    <div class="add-diet">
+                        <label>Dinner</label>
+                        <textarea
+                            placeholder="e.g. A tuna salad with greens and olive oil, as well as a fruit salad"></textarea>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="add-button">
-            <AddIcon />
-            <button type="button" @click="addDay">Add Day</button>
-        </div>
+            <div class="add-button">
+                <AddIcon />
+                <button type="button" @click="addDay">Add Day</button>
+            </div>
 
-        <div class="add-diet description">
-            <label for="description">Description</label>
-            <textarea name="description" placeholder="e.g. The Mediterranean diet is a way of eating that's based on the traditional cuisines of Greece, Italy and other countries that border the Mediterranean Sea."></textarea>
-        </div>
+            <div class="add-diet description">
+                <label for="description">Description</label>
+                <textarea name="description"
+                    placeholder="e.g. The Mediterranean diet is a way of eating that's based on the traditional cuisines of Greece, Italy and other countries that border the Mediterranean Sea."></textarea>
+            </div>
 
-        <div class="add-diet-btns">
-            <button type="button" class="w-btn w-btn-close" @click="close">
-                Cancel
-            </button>
-            <button type="submit" class="w-btn">
-                Save
-            </button>
+            <div class="add-diet-btns">
+                <button type="button" class="w-btn w-btn-close" @click="close">
+                    Cancel
+                </button>
+                <button type="submit" class="w-btn">
+                    Save
+                </button>
+            </div>
         </div>
     </div>
 </template>
--- src/views/Diet/AddRecipeView.vue
+++ src/views/Diet/AddRecipeView.vue
@@ -4,19 +4,19 @@ import CameraIcon from '@/components/icons/IconCamera.vue'
 
 export default {
     components: {
-        AddIcon, 
+        AddIcon,
         CameraIcon
     },
     data() {
         return {
-            ingredients: 3, 
+            ingredients: 3,
             steps: 3,
         }
     },
     methods: {
         addIngredient: function () {
             if (this.ingredients < 10) this.ingredients++;
-        }, 
+        },
         addStep: function () {
             if (this.steps < 6) this.steps++;
         }
@@ -29,74 +29,77 @@ export default {
         <div class="layout-wrapper">
             <h3>Add New Recipe</h3>
         </div>
-    </div>
 
-    <div class="add-diet-wrapper page-bg">
-        <div class="add-diet-head">
-            <div class="add-diet upload-photo">
-                <label>Upload Photo</label>
-                <div>
-                    <CameraIcon />
-                </div>
-            </div>
 
-            <div class="add-diet-inner">
-                <div class="add-diet">
-                    <label for="diet">Recipe Title</label>
-                    <input name="diet" type="text" placeholder="e.g. Lemon chicken" />
+        <div class="add-diet-wrapper page-bg">
+            <div class="add-diet-head">
+                <div class="add-diet upload-photo">
+                    <label>Upload Photo</label>
+                    <div>
+                        <CameraIcon />
+                    </div>
                 </div>
-                
-                <div class="add-diet">
-                    <label for="diet">Servings</label>
-                    <input name="diet" type="text" placeholder="e.g. 6" />
-                </div>
-                
-                <div class="add-diet">
-                    <label for="duration">Cook Time</label>
-                    <input name="duration" type="text" placeholder="e.g. 20 min." />
-                </div>
-            </div>
-        </div>
 
-        <div>
-            <div class="add-diet-day-grid-item title">Add Ingredients</div>
-            <div class="add-diet-day-grid">
-                <div class="add-diet-day-grid-item" v-for="ingredient in ingredients" :key="ingredient" :id="ingredient">
+                <div class="add-diet-inner">
                     <div class="add-diet">
-                        <label>Ingredient</label>
-                        <input type="text" placeholder="e.g. 200gr quinoa" />
+                        <label for="diet">Recipe Title</label>
+                        <input name="diet" type="text" placeholder="e.g. Lemon chicken" />
+                    </div>
+
+                    <div class="add-diet">
+                        <label for="diet">Servings</label>
+                        <input name="diet" type="text" placeholder="e.g. 6" />
+                    </div>
+
+                    <div class="add-diet">
+                        <label for="duration">Cook Time</label>
+                        <input name="duration" type="text" placeholder="e.g. 20 min." />
                     </div>
                 </div>
             </div>
-            <div class="add-button">
-                <AddIcon />
-                <button type="button" @click="addIngredient">Add Ingredient</button>
-            </div>
-        </div>
 
-        <div class="steps-wrapper">
-            <div class="add-diet-day-grid-item title">How to cook</div>
-            <div class="add-diet-day-grid">
-                <div class="add-diet-day-grid-item" v-for="(step, index) in steps" :key="step" :id="step">
-                    <div class="add-diet">
-                        <label>Step {{ index + 1 }}</label>
-                        <textarea placeholder="e.g. Bring 1 cup water to a boil in a pot, then add the quinoa. Cook for 15 minutes, until water is absorbed. Take off the heat and let cool."></textarea>
+            <div>
+                <div class="add-diet-day-grid-item title">Add Ingredients</div>
+                <div class="add-diet-day-grid">
+                    <div class="add-diet-day-grid-item" v-for="ingredient in ingredients" :key="ingredient"
+                        :id="ingredient">
+                        <div class="add-diet">
+                            <label>Ingredient</label>
+                            <input type="text" placeholder="e.g. 200gr quinoa" />
+                        </div>
                     </div>
                 </div>
+                <div class="add-button">
+                    <AddIcon />
+                    <button type="button" @click="addIngredient">Add Ingredient</button>
+                </div>
             </div>
-            <div class="add-button">
-                <AddIcon />
-                <button type="button" @click="addStep">Add Step</button>
-            </div>
-        </div>
 
-        <div class="add-diet-btns">
-            <button type="button" class="w-btn w-btn-close" @click="close">
-                Cancel
-            </button>
-            <button type="submit" class="w-btn">
-                Save
-            </button>
+            <div class="steps-wrapper">
+                <div class="add-diet-day-grid-item title">How to cook</div>
+                <div class="add-diet-day-grid">
+                    <div class="add-diet-day-grid-item" v-for="(step, index) in steps" :key="step" :id="step">
+                        <div class="add-diet">
+                            <label>Step {{ index + 1 }}</label>
+                            <textarea
+                                placeholder="e.g. Bring 1 cup water to a boil in a pot, then add the quinoa. Cook for 15 minutes, until water is absorbed. Take off the heat and let cool."></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="add-button">
+                    <AddIcon />
+                    <button type="button" @click="addStep">Add Step</button>
+                </div>
+            </div>
+
+            <div class="add-diet-btns">
+                <button type="button" class="w-btn w-btn-close" @click="close">
+                    Cancel
+                </button>
+                <button type="submit" class="w-btn">
+                    Save
+                </button>
+            </div>
         </div>
     </div>
 </template>
