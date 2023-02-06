fa60e15e5f707b1ab83fb81881039a664240290c
letovaatiga <letovaatiga@gmail.com>
2023-02-06 17:50:33 +0400

    Add quiz page style fixes

--- src/views/Quizzes/AddQuizView.vue
+++ src/views/Quizzes/AddQuizView.vue
@@ -20,76 +20,34 @@ export default {
     data() {
         const alertStore = useAlertStore();
         return {
-            quizId: null,
             alertStore,
-            quizData: null,
-            editor: ClassicEditor,
-            editorConfig: {
-                // The configuration of the editor.
-            }
-        }
-    },
-    watch: {
-        '$route.params': {
-            handler(toParams, previousParams) {
-                const id = toParams.id;
-                if (id) {
-                    this.getQuiz(id)
-                } else {
-                    this.initialQuiz()
-                }
-            },
-            immediate: true
-        }
-    },
-    methods: {
-        initialQuiz() {
-            this.quizId = null
-            this.quizData = {
-                title: '',
-                article: '',
+            formData: {
                 questions: [
                     {
                         text: '',
                         answers: ['', '', '']
                     }
                 ]
+            },
+            editor: ClassicEditor,
+            editorConfig: {
+                // The configuration of the editor.
             }
-        },
+        }
+    },
+    mounted() {
+    },
+    methods: {
         submitQuiz(values) {
-            if (this.quizId) {
-                axiosInstance.put(`/quizzes/${this.quizId}`, values)
-                    .then(response => {
-                        this.alertStore.success('Quiz updated')
-                        this.$router.back()
-                    })
-                    .catch(error => {
-                        console.log(error)
-                        this.alertStore.error(error.response.data.message)
-                    });
-            } else {
-                axiosInstance.post('/quizzes', values)
-                    .then(response => {
-                        this.alertStore.success('Quiz added')
-                        this.$router.back()
-                    })
-                    .catch(error => {
-                        console.log(error)
-                        this.alertStore.error(error.response.data.message)
-                    });
-            }
-
-        },
-        getQuiz(id) {
-            axiosInstance.get(`/quizzes/${id}`)
+            console.log(values)
+            axiosInstance.post('/quizzes', values)
                 .then(response => {
-                    this.quizData = response.data.quiz;
-                    this.quizId = this.quizData.id
+                    this.alertStore.success('Quotes added')
+                    this.$router.back()
                 })
                 .catch(error => {
+                    console.log(error)
                     this.alertStore.error(error.response.data.message)
-                    this.$router.replace({ name: 'add-quiz' })
-
                 });
         },
         close() {
@@ -106,7 +64,7 @@ export default {
         </div>
 
         <div class="addquiz-wrapper page-bg">
-            <Form v-if="quizData" @submit="submitQuiz" :initial-values="quizData">
+            <Form @submit="submitQuiz" :initial-values="formData">
                 <!-- <div class="addquote-selects">
                     <div class="popup-content-item popup-content-item--select">
                         <label>Select Patients</label>
@@ -157,39 +115,39 @@ export default {
                     </div>
 
                     <div class="editor-container">
-                        <Field name="article">
-                            <ckeditor :editor="editor" v-model="quizData.article" :config="editorConfig"></ckeditor>
+                        <Field name="article" v-slot="{ field }">
+                            <ckeditor :editor="editor" v-bind="field" :config="editorConfig"></ckeditor>
                         </Field>
                     </div>
 
                     <h4>Questions</h4>
 
                     <FieldArray :name="`questions`" v-slot="{ fields, push, remove }">
-                        <fieldset v-for="(field, index) in fields" :key="field.key" class="addquiz-inner-input">
+                        <div class="addquiz-inner-grid">
+                            <fieldset v-for="(field, index) in fields" :key="field.key" class="addquiz-inner-item">
+                                <div class="addquiz-inner-item-head">
+                                    <div>Question {{ index + 1 }}</div>
+                                    <button v-if="fields.length > 1" type="button"
+                                        @click="remove(index)">Remove Question</button>
+                                </div>
 
-                            <div>Question {{ index + 1 }}</div> <button v-if="fields.length > 1" type="button"
-                                @click="remove(index)">X</button>
+                                <Field as="textarea" :name="`questions[${index}].text`" placeholder="Write a question..." />
 
-                            <Field as="textarea" :name="`questions[${index}].text`" placeholder="Write a question..." />
-
-                            <div>Answers
-                                <FieldArray :name="`questions[${index}].answers`" v-slot="{ fields, push, remove }">
-                                    <fieldset v-for="(a_field, idx) in fields" :key="a_field.key">
-                                        <div style="display:inline-flex">
-                                            <Field type="text" :name="`questions[${index}].answers[${idx}]`" />
+                                <div class="addquiz-inner-item-list">Answers
+                                    <FieldArray :name="`questions[${index}].answers`" v-slot="{ fields, push, remove }">
+                                        <fieldset v-for="(a_field, idx) in fields" :key="a_field.key">
                                             <Field type="radio" :name="`questions[${index}].correct`" :value="idx">
                                             </Field>
+                                            <Field type="text" :name="`questions[${index}].answers[${idx}]`" placeholder="Write a question..."/>
 
                                             <button v-if="fields.length > 2" type="button"
                                                 @click="remove(idx)">X</button>
-                                        </div>
-                                    </fieldset>
-                                    <button type="button" v-if="fields.length < 5" @click="push('')">add another
-                                        answer</button>
-                                </FieldArray>
-                            </div>
-
-                        </fieldset>
+                                        </fieldset>
+                                        <button type="button" v-if="fields.length < 5" @click="push('')">Add Another Answer</button>
+                                    </FieldArray>
+                                </div>
+                            </fieldset>
+                        </div>
 
                         <div class="add-button" v-if="fields.length < 20">
                             <AddIcon />
