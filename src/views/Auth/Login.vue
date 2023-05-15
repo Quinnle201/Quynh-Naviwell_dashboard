f7cc703186259293f391659ba4099d494d4fabfb
letovaatiga <letovaatiga@gmail.com>
2023-05-16 00:02:46 +0400

    Login clinic name fix

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -120,7 +120,6 @@ async function onSubmit() {
         font-weight: 500;
         font-size: 10px;
         line-height: 15px;
-        white-space: nowrap;
     }
 
     .login-form-img div span:first-child {
